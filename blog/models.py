3ed703f7f0b536f44a09ab1e94248896903db186
Dmitry Pikov <dmitrypikov@yandex.ru>
2025-08-12 19:40:01 +0400

    Fix errors in PR

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Article(models.Model):
     name = models.CharField(max_length=50, verbose_name='Заголовок')
     description = models.TextField(max_length=500, verbose_name='Содержимое')
-    photo = models.ImageField(upload_to='photo/', verbose_name='Изображение', blank=True, null=True)
+    photo = models.ImageField(upload_to='photo/blogs/', verbose_name='Изображение', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     publication_at = models.BooleanField(verbose_name='Признак публикации')
     quantity_count = models.PositiveIntegerField(verbose_name='Количество просмотров', default=0)
--- blog/urls.py
+++ blog/urls.py
@@ -8,7 +8,7 @@ app_name = BlogConfig.name
 urlpatterns = [
     path('', ArticleListView.as_view(), name='article_list'),
     path('article/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
-    path('article/create', ArticleCreateView.as_view(), name='article_create'),
+    path('article/create/', ArticleCreateView.as_view(), name='article_create'),
     path('article/<int:pk>/update/', ArticleUpdateView.as_view(), name='article_update'),
     path('article/<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete')
 ]
