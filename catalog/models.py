3165a8be9632aed190fca6890ed5ab013051f74f
Dmitry Pikov <dmitrypikov@yandex.ru>
2025-09-06 15:01:39 +0400

    Add permissions to users

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from users.models import User
+
+
 class Category(models.Model):
     name = models.CharField(max_length=50, verbose_name='Наименование')
     description = models.TextField(max_length=500, verbose_name='Описание')
@@ -19,10 +22,15 @@ class Product(models.Model):
     price = models.IntegerField(verbose_name='Цена за покупку')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата последнего изменения')
+    publication_status = models.BooleanField(default=False, verbose_name='Статус публикации')
+    owner = models.ForeignKey(User, verbose_name='Владелец', help_text='Владелец продукта', blank=True, null=True, on_delete=models.SET_NULL)
 
     class Meta:
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
+        permissions = [
+            ("can_unpublish_product", "Can unpublish product"),
+        ]
 
     def __str__(self):
         return self.name
